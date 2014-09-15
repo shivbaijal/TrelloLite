f552b767f76234278db6455270e4d4a7688f9804
Shiv Baijal <shiv@shivbaijal.com>
2014-09-15 01:34:06 -0400

    Initial Commit

---
+++ TrelloLite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrelloLite
+//
+//  Created by Shiv Baijal on 2014-09-15.
+//  Copyright (c) 2014 Shiv Baijal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
